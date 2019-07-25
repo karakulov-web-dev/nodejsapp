ca690cf8837c9f6707f96784cd3c05a160d31ce8
karalulov.web.dev <karalulov.web.dev@gmail.com>
2019-07-25 10:04:45 +0700

    v 1.1.0

--- nodejsapp.js
+++ nodejsapp.js
@@ -2,16 +2,17 @@ const express = require("express");
 const proxy = require("http-proxy-middleware");
 
 const app = express();
-app.use(express.json());
+
+app.use("/nodejsapp/it-works", (req, res) => {
+  res.send("it works!");
+});
 
 app.use(
   "/nodejsapp/instructions",
   proxy({
     target: "http://212.77.128.203:8005/",
     changeOrigin: true,
-    pathRewrite: function(path, req) {
-      return path.replace(/^\/nodejsapp\/instructions/, "");
-    }
+    pathRewrite: path => path.replace(/^\/nodejsapp\/instructions/, "")
   })
 );
 
@@ -20,9 +21,25 @@ app.use(
   proxy({
     target: "http://212.77.128.203:8006/",
     changeOrigin: true,
-    pathRewrite: function(path, req) {
-      return path.replace(/^\/nodejsapp\/proxy/, "");
-    }
+    pathRewrite: path => path.replace(/^\/nodejsapp\/proxy/, "")
+  })
+);
+
+app.use(
+  "/nodejsapp/speechKit",
+  proxy({
+    target: "http://localhost:8081/",
+    changeOrigin: true,
+    pathRewrite: path => path.replace(/^\/nodejsapp\/speechKit/, "")
+  })
+);
+
+app.use(
+  "/nodejsapp/log",
+  proxy({
+    target: "http://localhost:8007/",
+    changeOrigin: true,
+    pathRewrite: path => path.replace(/^\/nodejsapp\/log/, "/log")
   })
 );
 
